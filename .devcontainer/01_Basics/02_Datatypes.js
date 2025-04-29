766f2a02dc8b88ceddaef6a6c349c041ed7aae5a
Pawan Bhirawandekar <pawanbhirawandekar@gmail.com>
2025-04-29 15:04:52 +0000

    Datatypes done

---
+++ .devcontainer/01_Basics/02_Datatypes.js
@@ -0,0 +1,20 @@
+//Datatypes
+/*
+**Premitive data type**
+number 
+bigint
+string => ""
+boolean => true or faslse
+null => standalone value
+undefined => value is not assigned to a varaible
+symbol =>unique
+
+
+**Object**
+typeOf
+
+*/
+let age = 20
+let name = "Pawan"
+console.log(age)
+console.log(name)
